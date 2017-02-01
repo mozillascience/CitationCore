ef7a5792ff57795accfa4acd91fefa728b81b3ee
Robert Lowe <rob@lowefrequency.com>
2017-01-31 23:34:25 -0500

    Modified formatted string to be generated once per set of tests

--- test/styles/bibtex.js
+++ test/styles/bibtex.js
@@ -1,3 +1,5 @@
+/* global it describe */
+
 const assert = require('assert');
 const bibtexmisc = require('../../model/styles/bibtexMisc');
 const biblatexSoftware = require('../../model/styles/biblatexSoftware');
@@ -7,63 +9,56 @@ const threeAuthorsSourceData = require('../mockSourceData/allFieldsThreeAuthors'
 
 describe('BibTexMiscStyle', () => {
   describe('format', () => {
+    const formattedString = bibtexmisc.format(oneAuthorSourceData);
     it('Month field, one author', () => {
-      const formattedString = bibtexmisc.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('month = {11},'), true);
     });
 
     it('Title field one author', () => {
-      const formattedString = bibtexmisc.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('title = {Red Barchetta},'), true);
     });
 
     it('Author field, one author', () => {
-      const formattedString = bibtexmisc.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('author = {Lee, Geddy},'), true);
     });
 
     it('Note field, one author', () => {
-      const formattedString = bibtexmisc.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('note = {Version: 1.0.0, URL: http://myUnclesFarm.com}'), true);
     });
     // Three Authors
+    const formattedStringThreeAuthor = bibtexmisc.format(threeAuthorsSourceData);
     it('Author field, three authors', () => {
-      const formattedString = bibtexmisc.format(threeAuthorsSourceData);
-      assert.equal(formattedString.includes('author = {Lee, Geddy and Lifeson, Alex and Peart, Neil},'), true);
+      assert.equal(formattedStringThreeAuthor.includes('author = {Lee, Geddy and Lifeson, Alex and Peart, Neil},'), true);
     });
   });
 });
 
 describe('BibLatexSoftwareStyle', () => {
   describe('format', () => {
+    const formattedString = biblatexSoftware.format(oneAuthorSourceData);
     it('Month field, one author', () => {
-      const formattedString = biblatexSoftware.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('month = {11},'), true);
     });
 
     it('Title field one author', () => {
-      const formattedString = biblatexSoftware.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('title = {Red Barchetta},'), true);
     });
 
     it('Author field, one author', () => {
-      const formattedString = biblatexSoftware.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('author = {Lee, Geddy},'), true);
     });
 
     it('URL field, one author', () => {
-      const formattedString = biblatexSoftware.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('url = {http://myUnclesFarm.com},'), true);
     });
 
     it('Version field, one author', () => {
-      const formattedString = biblatexSoftware.format(oneAuthorSourceData);
       assert.equal(formattedString.includes('version = {1.0.0}'), true);
     });
       // Three Authors
+    const formattedStringThreeAuthor = biblatexSoftware.format(threeAuthorsSourceData);
     it('Author field, three authors', () => {
-      const formattedString = biblatexSoftware.format(threeAuthorsSourceData);
-      assert.equal(formattedString.includes('author = {Lee, Geddy and Lifeson, Alex and Peart, Neil},'), true);
+      assert.equal(formattedStringThreeAuthor.includes('author = {Lee, Geddy and Lifeson, Alex and Peart, Neil},'), true);
     });
   });
 });
